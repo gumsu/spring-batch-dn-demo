6c6b63e790a9de06d4644573461ba6e9bf2fad10
gumsu <chavi9@naver.com>
2025-06-14 00:40:42 +0900

    fix: reader 클래스 이동

--- src/main/java/com/example/dn/batch/config/DemoBatchConfiguration.java
+++ src/main/java/com/example/dn/batch/config/DemoBatchConfiguration.java
@@ -3,20 +3,26 @@ package com.example.dn.batch.config;
 import com.example.dn.batch.domain.Restaurant;
 import com.example.dn.batch.listener.BatchExecutionLoggerListener;
 import com.example.dn.batch.CsvRangePartitioner;
-import com.example.dn.batch.RestaurantItemReader;
 import com.example.dn.batch.RestaurantItemWriter;
 import java.sql.SQLException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -28,7 +34,6 @@ public class DemoBatchConfiguration {
     private final JobRepository jobRepository;
     private final PlatformTransactionManager platformTransactionManager;
     private final @Lazy BatchExecutionLoggerListener batchExecutionLoggerListener;
-    private final RestaurantItemReader restaurantItemReader;
     private final RestaurantItemWriter restaurantItemWriter;
 
     private static final int CHUNK_SIZE = 100;
@@ -57,12 +62,75 @@ public class DemoBatchConfiguration {
     public Step slaveStep() throws Exception {
         return new StepBuilder("slaveStep", jobRepository)
             .<Restaurant, Restaurant>chunk(CHUNK_SIZE, platformTransactionManager)
-            .reader(restaurantItemReader.reader(0, 0))
+            .reader(reader(0, 0))
             .writer(restaurantItemWriter.writer())
             .listener(batchExecutionLoggerListener)
             .faultTolerant()
             .retryLimit(3)
             .retry(SQLException.class)
             .build();
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader<Restaurant> reader(
+        @Value("#{stepExecutionContext['startLine']}") int startLine,
+        @Value("#{stepExecutionContext['endLine']}") int endLine) {
+        FlatFileItemReader<Restaurant> reader = new FlatFileItemReader<>() {
+            private int currentLine = 0;
+
+            @Override
+            public Restaurant read() throws Exception {
+                Restaurant item;
+                do {
+                    item = super.read();
+                    currentLine ++;
+                } while (item != null && (currentLine < startLine || currentLine > endLine));
+
+                return (currentLine > endLine) ? null : item;
+            }
+        };
+
+        reader.setResource(new ClassPathResource("sample.csv"));
+        reader.setLinesToSkip(1);
+        reader.setEncoding("UTF-8");
+
+        /* defaultLineMapper: 읽으려는 데이터 LineMapper을 통해 Dto로 매핑 */
+        DefaultLineMapper<Restaurant> defaultLineMapper = new DefaultLineMapper<>();
+
+        /* delimitedLineTokenizer : txt 파일에서 구분자 지정하고 구분한 데이터 setNames를 통해 각 이름 설정 */
+        DelimitedLineTokenizer delimitedLineTokenizer = new CustomDelimitedLineTokenizer();
+        delimitedLineTokenizer.setDelimiter(",");
+
+        delimitedLineTokenizer.setNames(
+            "sequence", "openServiceName",	"openServiceId", "municipalityCode", "managementNumber", "permissionDate",
+            "cancellationDate",	"businessStatusCode", "businessStatusName",	"detailedBusinessStatusCode", "detailedBusinessStatusName",
+            "closureDate", "suspensionStartDate",	"suspensionEndDate", "reopenDate", "contactNumber", "siteArea", "postalCode",
+            "fullAddress", "roadNameAddress", "roadNamePostalCode", "businessName", "lastModifiedDate", "dataUpdateType", "dataUpdateDate",
+            "businessCategory", "coordinateX", "coordinateY", "sanitationType", "numberOfMaleEmployees", "numberOfFemaleEmployees",
+            "businessAreaType", "grade", "waterSupplyType", "totalEmployees", "headOfficeEmployees"	, "factoryOfficeEmployees", "factorySalesEmployees",
+            "factoryProductionEmployees", "buildingOwnershipType",	"securityDeposit", "monthlyRent", "isMultiUseFacility",	"totalFacilitySize",
+            "traditionalBusinessDesignationNumber", "mainDishOfTraditionalBusiness", "homepage" );//행으로 읽은 데이터 매칭할 데이터 각 이름
+        defaultLineMapper.setLineTokenizer(delimitedLineTokenizer); //lineTokenizer 설정
+
+        /* beanWrapperFieldSetMapper: 매칭할 class 타입 지정 */
+        BeanWrapperFieldSetMapper<Restaurant> beanWrapperFieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        beanWrapperFieldSetMapper.setTargetType(Restaurant.class);
+        beanWrapperFieldSetMapper.setStrict(false);
+
+        defaultLineMapper.setFieldSetMapper(beanWrapperFieldSetMapper); //fieldSetMapper 지정
+
+        reader.setLineMapper(defaultLineMapper); //lineMapper 지정
+        return reader;
+    }
+
+    static class CustomDelimitedLineTokenizer extends DelimitedLineTokenizer {
+
+        private static final char DEFAULT_QUOTE_CHARACTER = '"';
+
+        @Override
+        protected boolean isQuoteCharacter(char c) {
+            return c == DEFAULT_QUOTE_CHARACTER;
+        }
     }
 }
